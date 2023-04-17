7713947023356161e53d35eefe59f179579805c8
Peter Chibunna <peterchibunna@gmail.com>
2023-04-17 21:05:38 +0100

    Task #9.2

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,11 +3,9 @@ import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
+  return {
     ...budget,
     getIncomeInDollars: (income) => `$${income}`,
     getIncomeInEuros: (income) => `${income} euros`,
   };
-
-  return fullBudget;
 }
