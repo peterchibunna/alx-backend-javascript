06c81dee177b370dbeef026be99f0eae6e65ef14
Peter Chibunna <peterchibunna@gmail.com>
2023-04-17 19:01:36 +0100

    Task #1.3

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-export const taskFirst = () => {
+export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
-};
+}
 
 export function getLast() {
   return ' is okay';
 }
 
-export const taskNext = () => {
+export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
-};
+}
