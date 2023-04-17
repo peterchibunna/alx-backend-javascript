17c2dab4e519eb347953f587825cac2c7b51f3f0
Peter Chibunna <peterchibunna@gmail.com>
2023-04-17 20:18:51 +0100

    Task #4

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-export default function returnHowManyArguments() {
-  return arguments.length;
+export default function returnHowManyArguments(...args) {
+  return args.length;
 }
