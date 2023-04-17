904741269af52b39352f47883b597ae176026db2
Peter Chibunna <peterchibunna@gmail.com>
2023-04-17 19:23:39 +0100

    Task #0, #1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-   var task = false;
-   var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-     let task = true;
-     let task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
