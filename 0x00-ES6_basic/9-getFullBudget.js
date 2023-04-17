958b931b65013c6b38c520f2d8bf0a261010e0c1
Peter Chibunna <peterchibunna@gmail.com>
2023-04-17 20:49:59 +0100

    Task #9

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
