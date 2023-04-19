d9e5bd504511b5cfe54331bb18847d5feb333182
Peter Chibunna <peterchibunna@gmail.com>
2023-04-19 09:52:20 +0100

    correct lint for #1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,8 @@
 export default function getResponseFromAPI(success) {
   const successData = { status: 200, body: 'Success' };
   const errorData = 'The fake API is not working currently';
-  return new Promise((resolve, reject)=>{
-    if ( success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
       resolve(successData);
     } else {
       reject(Error(errorData));
