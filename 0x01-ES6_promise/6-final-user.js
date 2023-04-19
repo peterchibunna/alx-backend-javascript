13e2fef20ad3c7c39755e59f58b35a5b7c8bb956
Peter Chibunna <peterchibunna@gmail.com>
2023-04-19 11:39:52 +0100

    Task 6 correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => (
       response.map((data) => ({
         status: data.status,
         value: data.status === 'rejected' ? data.reason.toString() : data.value,
-      }));
-    });
+      }))
+    ));
 }
