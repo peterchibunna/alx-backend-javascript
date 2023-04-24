9cec4b649162b9ac28f294d2741a25e27984fa1e
Peter Chibunna <peterchibunna@gmail.com>
2023-04-24 19:16:36 +0100

    8. Clean set, correction?

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
   if (startString !== '' && set instanceof Set) {
     const arr = [];
+    if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+      return '';
+    }
     set.forEach((item) => {
       if (item.startsWith(startString)) {
         arr.push(item.replace(startString, ''));
