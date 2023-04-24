3698264895ec88029683bd2c05c3be1691bb3974
Peter Chibunna <peterchibunna@gmail.com>
2023-04-24 16:55:46 +0100

    10. More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, item) => {
+    if (value === 1) {
+      map.set(item, 100);
+    }
+  });
+}
