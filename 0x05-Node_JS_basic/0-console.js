f54555c0036c2b12af52949d65c6f3d4c58139bb
Peter Chibunna <peterchibunna@gmail.com>
2023-06-13 20:12:38 +0100

    initial commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+// #!/usr/bin/node
+/**
+ * function to display whatever argument to the STDOUT
+ * @param value
+ */
+const displayMessage = (value) => {
+  console.log(value);
+};
+
+module.exports = displayMessage;
