56540e52115ba9686e3f52e3e3f44906a09adf87
Peter Chibunna <peterchibunna@gmail.com>
2023-06-13 21:03:49 +0100

    2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+function countStudents(filename) {
+  if (fs.existsSync(filename) && fs.statSync(filename).isFile()) {
+    const data = fs.readFileSync(filename, {encoding: 'utf8', flag: 'r'});
+    const lines = data.split('\n');
+    const fields = {};
+    let firstNames = {};
+    let numStudents = -1; // 1st line is a header
+    lines.forEach(function (line) {
+      if (line.trim() !== "") {
+        const fieldName = line.split(',')[3];
+        const firstName = line.split(',')[0];
+        if(fieldName !== 'field') {
+          if (Object.keys(fields).includes(fieldName)) {
+            fields[fieldName] += 1;
+            firstNames[fieldName].push(firstName);
+          } else {
+            fields[fieldName] = 1;
+            firstNames[fieldName]= [firstName];
+          }
+        }
+        numStudents += 1;
+      }
+    });
+    console.log(`Number of students: ${numStudents}`);
+    for (const key in fields) {
+      if (fields.hasOwnProperty(key)) {
+        console.log(`Number of students in ${key}: ${fields[key]}. List: ${firstNames[key].join(', ')}`);
+      }
+    }
+  } else {
+    throw new Error('Cannot load the database');
+  }
+
+
+}
